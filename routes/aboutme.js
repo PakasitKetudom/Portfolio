ecdffeb16b823d64a91ac274e394869b80ed9506
PakasitKetudom <ke.pakasit@gmail.com>
2020-12-22 16:57:17 +0700

    update aboutme and fix other

--- routes/aboutme.js
+++ routes/aboutme.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('aboutme', { title: 'about me' });
+  res.render('aboutme', { title: 'About Me' });
 });
 
 module.exports = router;
